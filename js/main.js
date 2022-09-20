d35c5f3c3ef31b504018062648307b3cb10e019f
Rohit Saini <rohit@techfnatic.com>
2022-09-20 19:07:29 +0530

    create product page, career page, contact page and home page complete

--- js/main.js
+++ js/main.js
@@ -1,32 +1,22 @@
 // ===== Index Page Product Slider ====
 $('.product-slider').slick({
     dots: true,
-    infinite: false,
+    arrow: true,
+    infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 992,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 2
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToShow: 1
         }
       }
     ]
